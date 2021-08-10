ed79d608993edc2730040f116e02b41ab15d966f
christianlopez <christian_lopez@avansys.com.mx>
2021-08-09 21:15:25 -0500

    Resolución del reto

--- src/VillaPeruana.php
+++ src/VillaPeruana.php
@@ -10,6 +10,8 @@ class VillaPeruana
 
     public $sellIn;
 
+    public $qualityFactor;
+
     public function __construct($name, $quality, $sellIn)
     {
         $this->name = $name;
@@ -23,51 +25,43 @@ class VillaPeruana
 
     public function tick()
     {
-        if ($this->name != 'Pisco Peruano' and $this->name != 'Ticket VIP al concierto de Pick Floid') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Tumi de Oro Moche') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
+        //Definimos el factor de ajuste de calidad
+        $this->qualityFactor=1;
+        if($this->name == 'Café Altocusco') $this->qualityFactor=-2;
+        if($this->name == 'normal') $this->qualityFactor=-1;
 
-                if ($this->name == 'Ticket VIP al concierto de Pick Floid') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
+        //Solo se modifican los atributos de los productos que NO son Tumi
+        if ($this->name != 'Tumi de Oro Moche')
+        {
+            if($this->quality>0 && $this->quality<50)
+            {
+                //La calidad siempre se moverá al menos una vez en función del producto
+                $this->quality+=$this->qualityFactor;
+                //Lo tickets VIP tienen un comportamiento distinto al resto de los productos
+                if($this->name == 'Ticket VIP al concierto de Pick Floid')
+                {
+                    if($this->sellIn>0)
+                    {
+                        if($this->sellIn<=10)
+                            $this->quality+=$this->qualityFactor;
+                        if($this->sellIn<=5)
+                            $this->quality+=$this->qualityFactor;
                     }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
+                    else {
+                        $this->quality=0;
                     }
                 }
-            }
-        }
+                //El resto de los productos se comportan igual
+                else
+                {
+                    if($this->sellIn<=0)
+                        $this->quality+=$this->qualityFactor;
+                }
 
-        if ($this->name != 'Tumi de Oro Moche') {
-            $this->sellIn = $this->sellIn - 1;
-        }
-
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Pisco Peruano') {
-                if ($this->name != 'Ticket VIP al concierto de Pick Floid') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Tumi de Oro Moche') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
+                if($this->quality>50)
+                    $this->quality=50;
             }
+            $this->sellIn -=  1;
         }
     }
 }
